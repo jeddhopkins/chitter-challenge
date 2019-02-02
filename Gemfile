76e954158e15b55062e3f4d723805e1be7618410
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-02-02 14:56:56 +0000

    gemfile and folder structure setup

--- Gemfile
+++ Gemfile
@@ -1,12 +1,21 @@
 source 'https://rubygems.org'
 
-ruby '2.5.0'
+ruby '2.5.1'
 
+gem 'bcrypt'
+gem 'data_mapper', '1.2.0'
+gem 'dm-postgres-adapter'
+gem 'launchy'
 gem 'rake'
-gem 'rubocop', '0.56.0'
+gem 'rb-readline'
+gem 'sinatra-flash'
 
 group :test do
+  gem 'capybara'
   gem 'rspec'
+  gem 'rubocop', '0.56.0'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem 'sinatra'
+  gem 'timecop'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara/rspec'
+require 'sinatra'
+require 'rspec'
+require 'timecop'
+require_relative './../app.rb'
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -9,9 +15,10 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
-  config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+
+  config.include Rack::Test::Methods
+  config.before(:each) do
+    DataMapper.setup(:default, 'postgres://localhost/chitter_test')
+    DataMapper.finalize
+    DataMapper.auto_migrate!
   end
-end
